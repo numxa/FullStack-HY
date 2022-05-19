3b54b059e8e33b51240db18090fc476c12c23d54
Olli Numminen <olli.numminen@tuni.fi>
2022-05-19 14:39:29 +0300

    Ohjelman muuttujamäärittelyt olioiksi ja muun ohjelman mukauttaminen

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function Course(props) {
+  console.log(props)
   return (
     <h1>{props.course}</h1>
   )
@@ -38,22 +39,29 @@ const Part = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
       <Course course={course}/>
 
-      <Content part1 = {part1} exercises1 = {exercises1}/>
-      <Content part2 = {part2} exercises2 = {exercises2}/>
-      <Content part3 = {part3} exercises3 = {exercises3}/>
+      <Content part1 = {part1.name} exercises1 = {part1.exercises}/>
+      <Content part2 = {part2.name} exercises2 = {part2.exercises}/>
+      <Content part3 = {part3.name} exercises3 = {part3.exercises}/>
 
-      <Total total = {exercises1 + exercises2 + exercises3} />
+      <Total total = {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
